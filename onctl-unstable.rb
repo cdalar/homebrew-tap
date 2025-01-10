b2dfc333760be0c2d28d123db67a78f2d5d9c38a
Cemal Y. Dalar <cemal@dalar.net>
2025-01-10 06:00:59 +0100

    add onctl-unstable

---
+++ onctl-unstable.rb
@@ -0,0 +1,14 @@
+class OnctlUnstable < Formula
+  desc "Unstable version of the onctl tool"
+  homepage "https://github.com/cdalar/onctl"
+  head "https://github.com/cdalar/onctl.git", branch: "main"
+
+  def install
+    system "make"
+    bin.install "onctl" # Replace "onctl" with the actual name of the built binary
+  end
+
+  test do
+    system "#{bin}/onctl", "version" # Replace with an appropriate test for your tool
+  end
+end
